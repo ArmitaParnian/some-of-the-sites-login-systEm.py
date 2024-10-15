57d7d0cddcacc54d51cb9cd14e93217752ad2f39
ArmitaParnian <parnianarmita4@gmail.com>
2024-10-15 02:24:45 -0700

    Add files via upload

---
+++ some of the sites login system.py.py
@@ -0,0 +1,24 @@
+
+import re
+import sys
+
+
+
+def main():
+    email = input("what's your name?").strip()
+    if re.search(r"^\w+@\w+\.(com|edu|gov|net|org|youtube)$",email):
+        print("Valid")
+
+        print((input("HTML: ")))
+        youtube = input("what's your link?").strip()
+        username,domain = youtube.split("//")
+        if username and "." in domain:
+           print()
+
+
+    else :
+        print("Invalid")
+
+
+if __name__ == "__main__":
+    main()
